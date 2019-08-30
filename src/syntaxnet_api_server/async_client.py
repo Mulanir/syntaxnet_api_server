028d9549155b9777583f9c7be9dfd3f42c45464a
keltam <bodiasyd@gmail.com>
2019-08-30 16:24:21 +0300

    CLient

---
+++ src/syntaxnet_api_server/async_client.py
@@ -0,0 +1,10 @@
+import socket
+
+
+if __name__ == "__main__":
+    socket = socket.create_connection(('localhost', 8111))
+
+    while True:
+        message = input()
+
+        socket.send(message)
